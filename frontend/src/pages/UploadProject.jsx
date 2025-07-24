fb56b4a618209c131d7ef104d2b839453eec3ce5
vrushal <vrushal.com@gmail.com>
2025-07-24 20:29:28 +0530

    removed old backend

--- frontend/src/pages/UploadProject.jsx
+++ frontend/src/pages/UploadProject.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import axios from "../api/axios";
+// import axios from "../api/axios";
 
 function UploadProject() {
   const [formData, setFormData] = useState({
