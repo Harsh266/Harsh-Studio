f5fb57022bd5edfa180dc150d152bfc4576ba58f
Harsh Vekriya <harshvekariya203@gmail.com>
2025-08-05 16:53:22 +0530

    add analytics feacture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,16 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import UploadProject from "./pages/UploadProject";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
+    
     <ThemeProvider>
+      <Analytics />
       <Router>
         <Routes>
+          
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
